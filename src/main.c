f9cbce595286f7a0906bd8ee2a5746b0557e2da8
Arjun Vikram <arjvik@gmail.com>
2021-03-21 19:55:16 -0500

    Working Hello World

--- src/main.c
+++ src/main.c
@@ -1,13 +1,19 @@
-/*
- *--------------------------------------
- * Program Name:
- * Author:
- * License:
- * Description:
- *--------------------------------------
-*/
+#include <tice.h>
+#include <stdlib.h>
 
+/* Main function, called first */
 int main(void)
 {
+    /* Clear the homescreen */
+    os_ClrHome();
+
+    /* Print a string */
+    os_PutStrFull("Hello, World.");
+
+    /* Waits for a key */
+    while (!os_GetCSC());
+
+    /* Return 0 for success */
     return 0;
 }
+
