3a992ff592a7cba3e00c633b0005a39bb076d4fa
Arjun Vikram <arjvik@gmail.com>
2021-03-21 19:56:32 -0500

    Keypad input sample

--- src/main.c
+++ src/main.c
@@ -1,19 +1,44 @@
 #include <tice.h>
-#include <stdlib.h>
+#include <keypadc.h>
 
-/* Main function, called first */
+/* Function prototypes */
+void PrintText(int8_t xpos, int8_t ypos, const char *text);
+
 int main(void)
 {
+    /* Key variable */
+    kb_key_t key;
+    const char *erase_string = "     ";
+
     /* Clear the homescreen */
     os_ClrHome();
 
-    /* Print a string */
-    os_PutStrFull("Hello, World.");
+    /* Loop until 2nd is pressed */
+    do
+    {
+        /* Update kb_Data */
+        kb_Scan();
 
-    /* Waits for a key */
-    while (!os_GetCSC());
+        key = kb_Data[7];
 
-    /* Return 0 for success */
+        /* Print the current arrow key input */
+        PrintText(0, 0, key & kb_Down ? "Down" : erase_string);
+
+        PrintText(0, 1, key & kb_Up ? "Up" : erase_string);
+
+        PrintText(0, 2, key & kb_Left ? "Left" : erase_string);
+
+        PrintText(0, 3, key & kb_Right ? "Right" : erase_string);
+
+    } while (kb_Data[1] != kb_2nd);
+
     return 0;
+}
+
+/* Draw text on the homescreen at the given X/Y location */
+void PrintText(int8_t xpos, int8_t ypos, const char *text)
+{
+    os_SetCursorPos(ypos, xpos);
+    os_PutStrFull(text);
 }
 
