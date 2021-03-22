d01eae14556a5833f0dad38b0d99f443cc2f57c4
Arjun Vikram <arjvik@gmail.com>
2021-03-21 20:02:38 -0500

    Custom input routine

--- src/main.c
+++ src/main.c
@@ -1,44 +1,65 @@
 #include <tice.h>
 #include <keypadc.h>
 
-/* Function prototypes */
-void PrintText(int8_t xpos, int8_t ypos, const char *text);
+void init_keys(uint8_t* last) {
+    kb_EnableOnLatch();
+    kb_ClearOnLatch();
+    kb_Scan();
+    for (uint8_t i = 0; i < 8; i++)
+        last[i] = kb_Data[i];
+}
 
-int main(void)
-{
-    /* Key variable */
-    kb_key_t key;
-    const char *erase_string = "     ";
+typedef enum {
+    key_NONE = 0,
+    key_0,
+    key_1,
+    key_2,
+    key_2nd
+} key_t;
 
-    /* Clear the homescreen */
+key_t get_key(uint8_t* last) {
+    kb_Scan();
+    key_t ret = key_NONE;
+    if         ((kb_Data[3] & ~last[3]) & kb_0)
+        ret = key_0;
+    else if ((kb_Data[3] & ~last[3]) & kb_1)
+        ret = key_1;
+    else if ((kb_Data[4] & ~last[4]) & kb_2)
+        ret = key_2;
+    else if ((kb_Data[1] & ~last[1]) & kb_2nd)
+        ret = key_2nd;
+    
+    for (uint8_t i = 1; i < 8; i++)
+        last[i] = kb_Data[i];
+    return ret;
+}
+
+void main() {
     os_ClrHome();
+    uint8_t last[8];
+    init_keys(last);
+    key_t key;
 
-    /* Loop until 2nd is pressed */
-    do
-    {
-        /* Update kb_Data */
-        kb_Scan();
-
-        key = kb_Data[7];
-
-        /* Print the current arrow key input */
-        PrintText(0, 0, key & kb_Down ? "Down" : erase_string);
-
-        PrintText(0, 1, key & kb_Up ? "Up" : erase_string);
-
-        PrintText(0, 2, key & kb_Left ? "Left" : erase_string);
-
-        PrintText(0, 3, key & kb_Right ? "Right" : erase_string);
-
-    } while (kb_Data[1] != kb_2nd);
-
-    return 0;
+    while (!kb_On) {
+	
+        key = get_key(last);
+        os_SetCursorPos(0, 0);
+        switch (key) {
+        case key_0:
+        	os_PutStrFull("0");
+        	break;
+    	case key_1:
+        	os_PutStrFull("1");
+        	break;
+    	case key_2:
+        	os_PutStrFull("2");
+        	break;
+        case key_2nd:
+        	os_PutStrFull("^");
+        	break;
+    	case key_NONE:
+    		os_PutStrFull(" ");
+    		break;
+        }
+    }
 }
-
-/* Draw text on the homescreen at the given X/Y location */
-void PrintText(int8_t xpos, int8_t ypos, const char *text)
-{
-    os_SetCursorPos(ypos, xpos);
-    os_PutStrFull(text);
-}
-
